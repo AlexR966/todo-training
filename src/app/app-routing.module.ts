3b8515c6270d610e4124a53075fe1935214218d2
AlexR966 <aleksandra.rutkowska@opoczta.pl>
2022-03-13 09:50:45 +0100

    zmiany

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageModule } from './pages/home.page-module';
 import { AboutUsPageModule } from './pages/about-us.page-module';
 import { EmployeeDetailsPageModule } from './pages/employee-details.page-module';
+import { EmployeesPageModule } from './pages/employees.page-module';
 
 const routes: Routes = [{ 
         path: 'home', 
@@ -15,6 +16,10 @@ const routes: Routes = [{
   { 
         path: 'pracownik/:name', 
         loadChildren: () => EmployeeDetailsPageModule
+      },
+  { 
+        path: 'pracownik', 
+        loadChildren: () => EmployeesPageModule
       }
 ];
 
